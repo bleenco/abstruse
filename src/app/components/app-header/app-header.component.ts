e0a8e1bf182be33fee5c22efd5c71aa8c37c0089
Jan Kuri <jkuri88@gmail.com>
2017-06-15 04:52:37 +0200

    feat(dropdown-menu): ui dropdown menu in header

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, HostListener, ElementRef } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -7,10 +7,44 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: 'app-header.component.html'
 })
 export class AppHeaderComponent {
-  constructor(private authService: AuthService, private router: Router) { }
+  menuDropped: boolean;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private elementRef: ElementRef
+  ) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.menuDropped = false;
+      }
+    });
+  }
+
+  toggleMenu() {
+    this.menuDropped = !this.menuDropped;
+  }
 
   logout(): void {
     this.authService.logout();
+    this.menuDropped = false;
     this.router.navigate(['/login']);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onBlur(e: MouseEvent) {
+    if (!this.menuDropped) {
+      return;
+    }
+
+    let toggleBtn = this.elementRef.nativeElement.querySelector('.user-item');
+    if (e.target === toggleBtn || toggleBtn.contains(<any>e.target)) {
+      return;
+    }
+
+    let dropMenu: HTMLElement = this.elementRef.nativeElement.querySelector('.nav-dropdown');
+    if (dropMenu && dropMenu !== e.target && !dropMenu.contains((<any>e.target))) {
+      this.menuDropped = false;
+    }
   }
 }
