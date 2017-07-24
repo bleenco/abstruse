4d3acaeee465fb80d5d289ef210acb9c6223d360
Izak Lipnik <izak.lipnik@gmail.com>
2017-07-24 09:07:48 +0200

    test(): added protractor e2e test for user registration

--- e2e/020_registration.e2e.ts
+++ e2e/020_registration.e2e.ts
@@ -7,13 +7,54 @@ describe('User Login', () => {
     browser.get('/');
   });
 
-  it(`shoud reject register when password don't match`, () => {
+  it(`continue button shoud be disabled when password is empty`, () => {
     browser.get('/setup')
       .then(() => delay(5000))
       .then(() => element(by.css('[name="btn-continue"]')).click())
       .then(() => element(by.css('.form-input[name="email"]')).sendKeys('john@gmail.com'))
       .then(() => element(by.css('.form-input[name="name"]')).sendKeys('John Wayne'))
       .then(() => element(by.css('.form-input[name="password"]')).sendKeys('test123'))
+      .then(() => element(by.css('.button[name="btn-register"]')).isEnabled())
+      .then(enabled => expect(enabled).toEqual(false))
+      .then(() => browser.getCurrentUrl())
+      .then(url => expect(url).toEqual('http://localhost:6500/setup'));
+  });
+
+  it(`continue button shoud be disabled when email is not email address`, () => {
+    browser.get('/setup')
+      .then(() => delay(5000))
+      .then(() => element(by.css('[name="btn-continue"]')).click())
+      .then(() => element(by.css('.form-input[name="email"]')).sendKeys('johngmail.com'))
+      .then(() => element(by.css('.form-input[name="name"]')).sendKeys('John Wayne'))
+      .then(() => element(by.css('.form-input[name="password"]')).sendKeys('test123'))
+      .then(() => element(by.css('.form-input[name="password2"]')).sendKeys('test123'))
+      .then(() => element(by.css('.button[name="btn-register"]')).isEnabled())
+      .then(enabled => expect(enabled).toEqual(false))
+      .then(() => browser.getCurrentUrl())
+      .then(url => expect(url).toEqual('http://localhost:6500/setup'));
+  });
+
+  it(`continue button shoud be disabled when name is empty`, () => {
+    browser.get('/setup')
+      .then(() => delay(5000))
+      .then(() => element(by.css('[name="btn-continue"]')).click())
+      .then(() => element(by.css('.form-input[name="email"]')).sendKeys('john@gmail.com'))
+      .then(() => element(by.css('.form-input[name="password"]')).sendKeys('test123'))
+      .then(() => element(by.css('.form-input[name="password2"]')).sendKeys('test123'))
+      .then(() => element(by.css('.button[name="btn-register"]')).isEnabled())
+      .then(enabled => expect(enabled).toEqual(false))
+      .then(() => browser.getCurrentUrl())
+      .then(url => expect(url).toEqual('http://localhost:6500/setup'));
+  });
+
+  it(`continue button shoud be disabled when passwords don't match`, () => {
+    browser.get('/setup')
+      .then(() => delay(5000))
+      .then(() => element(by.css('[name="btn-continue"]')).click())
+      .then(() => element(by.css('.form-input[name="email"]')).sendKeys('john@gmail.com'))
+      .then(() => element(by.css('.form-input[name="name"]')).sendKeys('John Wayne'))
+      .then(() => element(by.css('.form-input[name="password"]')).sendKeys('test123'))
+      .then(() => element(by.css('.form-input[name="password2"]')).sendKeys('test12'))
       .then(() => element(by.css('.button[name="btn-register"]')).isEnabled())
       .then(enabled => expect(enabled).toEqual(false))
       .then(() => browser.getCurrentUrl())
