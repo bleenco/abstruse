fbe06f887710ab350047eb8da0c8800692051b43
Jan Kuri <jkuri88@gmail.com>
2017-07-26 11:40:04 +0200

    fix(): multiple pull_request webhooks fixes

--- src/api/db/repository.ts
+++ src/api/db/repository.ts
@@ -64,7 +64,7 @@ export function getRepositories(userId: string): Promise<any[]> {
   });
 }
 
-export function addRepository(data: any): Promise<boolean> {
+export function addRepository(data: any): Promise<any> {
   return new Promise((resolve, reject) => {
     new Repository().save(data, { method: 'insert' }).then(result => {
       if (!result) {
@@ -147,21 +147,7 @@ export function createPullRequest(data: any): Promise<any> {
             user_id: data.base.user.id,
             user_avatar_url: data.base.user.avatar_url,
             user_url: data.base.user.url,
-            user_html_url: data.base.user.html_url,
-            head_github_id: data.head.repo.id,
-            head_clone_url: data.head.repo.clone_url,
-            head_html_url: data.head.repo.html_url,
-            head_default_branch: data.head.repo.default_branch,
-            head_name: data.head.repo.name,
-            head_full_name: data.head.repo.full_name,
-            head_description: data.head.repo.description,
-            head_private: data.head.repo.private,
-            head_fork: data.head.repo.fork,
-            head_user_login: data.head.user.login,
-            head_user_id: data.head.user.id,
-            head_user_avatar_url: data.head.user.avatar_url,
-            head_user_url: data.head.user.url,
-            head_user_html_url: data.head.user.html_url,
+            user_html_url: data.base.user.html_url
           };
 
           return addRepository(repoData);
@@ -179,6 +165,20 @@ export function createPullRequest(data: any): Promise<any> {
           head_ref: data.head.ref,
           head_sha: data.head.sha,
           message: data.title,
+          head_github_id: data.head.repo.id,
+          head_clone_url: data.head.repo.clone_url,
+          head_html_url: data.head.repo.html_url,
+          head_default_branch: data.head.repo.default_branch,
+          head_name: data.head.repo.name,
+          head_full_name: data.head.repo.full_name,
+          head_description: data.head.repo.description,
+          head_private: data.head.repo.private,
+          head_fork: data.head.repo.fork,
+          head_user_login: data.head.user.login,
+          head_user_id: data.head.user.id,
+          head_user_avatar_url: data.head.user.avatar_url,
+          head_user_url: data.head.user.url,
+          head_user_html_url: data.head.user.html_url,
           start_time: new Date(),
           repositories_id: repo.id
         };
@@ -195,14 +195,32 @@ export function createPullRequest(data: any): Promise<any> {
 
 export function synchronizePullRequest(data: any): Promise<any> {
   return new Promise((resolve, reject) => {
+    let repo = null;
     let buildData = null;
 
     new Repository().where({ github_id: data.base.repo.id }).fetch()
-      .then(repo => {
-        if (!repo) {
-          return Promise.resolve(repo);
+      .then(repository => {
+        if (!repository) {
+          return addRepository({
+            github_id: data.base.repo.id,
+            clone_url: data.base.repo.clone_url,
+            html_url: data.base.repo.html_url,
+            default_branch: data.base.repo.default_branch,
+            name: data.base.repo.name,
+            full_name: data.base.repo.full_name,
+            description: data.base.repo.description,
+            private: data.base.repo.private,
+            fork: data.base.repo.fork,
+            user_login: data.base.user.login,
+            user_id: data.base.user.id,
+            user_avatar_url: data.base.user.avatar_url,
+            user_url: data.base.user.url,
+            user_html_url: data.base.user.html_url
+          })
+          .then(addedRepo => repo = addedRepo);
         } else {
-          return Promise.resolve(repo.toJSON());
+          repo = repository.toJSON();
+          return Promise.resolve(repository.toJSON());
         }
       })
       .then(repoData => {
@@ -220,7 +238,21 @@ export function synchronizePullRequest(data: any): Promise<any> {
           user_id: data.base.user.id,
           user_avatar_url: data.base.user.avatar_url,
           user_url: data.base.user.url,
-          user_html_url: data.base.user.html_url,
+          user_html_url: data.base.user.html_url
+        };
+
+        return updateRepository(repoData);
+      })
+      .then(() => {
+        buildData = {
+          pr: data.number,
+          label: data.base.label,
+          ref: data.base.ref,
+          sha: data.base.sha,
+          head_label: data.head.label,
+          head_ref: data.head.ref,
+          head_sha: data.head.sha,
+          message: data.title,
           head_github_id: data.head.repo.id,
           head_clone_url: data.head.repo.clone_url,
           head_html_url: data.head.repo.html_url,
@@ -235,25 +267,11 @@ export function synchronizePullRequest(data: any): Promise<any> {
           head_user_avatar_url: data.head.user.avatar_url,
           head_user_url: data.head.user.url,
           head_user_html_url: data.head.user.html_url,
-        };
-
-        return updateRepository(repoData);
-      })
-      .then(repo => {
-        buildData = {
-          pr: data.number,
-          label: data.base.label,
-          ref: data.base.ref,
-          sha: data.base.sha,
-          head_label: data.head.label,
-          head_ref: data.head.ref,
-          head_sha: data.head.sha,
-          message: data.title,
           start_time: new Date(),
           repositories_id: repo.id
         };
 
-        return getHttpJsonResponse(repo.head_user_url);
+        return getHttpJsonResponse(data.head.user.url);
       })
       .then(userData => {
         buildData.author = userData.name;
