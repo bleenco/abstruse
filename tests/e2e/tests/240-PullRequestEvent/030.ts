2ca3aa75852f74094d280e467d3ae3617e65f5c6
Izak Lipnik <izak.lipnik@gmail.com>
2017-07-27 14:20:50 +0200

    test(): added event subtypes for PullRequestEvent

--- tests/e2e/tests/240-PullRequestEvent/030.ts
+++ tests/e2e/tests/240-PullRequestEvent/030.ts
@@ -1,10 +1,10 @@
-import { requestOpened, header } from '../../webhooks/PullRequestEvent';
+import { requestClosed, header } from '../../webhooks/PullRequestEvent';
 import { sendRequest } from '../../utils/utils';
 import * from 'http';
 
 export default function() {
   return Promise.resolve()
-    .then(() => sendRequest(requestOpened, header))
+    .then(() => sendRequest(requestClosed, header))
     .then(parsedBody => {
       // console.log(parsedBody);
       return Promise.resolve();
