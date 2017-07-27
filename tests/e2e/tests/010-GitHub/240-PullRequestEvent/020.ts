8ed960f65fda64108064812a5646832fbaf30d5f
Jan Kuri <jkuri88@gmail.com>
2017-07-27 19:50:55 +0200

    refactor(e2e-tests): move GitHub events to its one separate directory

---
+++ tests/e2e/tests/010-GitHub/240-PullRequestEvent/020.ts
@@ -0,0 +1,16 @@
+import { requestReopened, header } from '../../../webhooks/github/PullRequestEvent';
+import { sendRequest } from '../../../utils/utils';
+import * from 'http';
+
+export default function() {
+  return Promise.resolve()
+    .then(() => sendRequest(requestReopened, header))
+    .then(parsedBody => {
+      // console.log(parsedBody);
+      return Promise.resolve();
+    })
+    .catch(err => {
+      // console.log(err);
+      return Promise.reject(err);
+    });
+}
