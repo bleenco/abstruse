747a4b415259ca132c42941fea96bec3eb71bd51
Jan Kuri <jkuri88@gmail.com>
2017-08-07 03:19:56 +0200

    test(e2e-protractor): build detals tests

--- protractor.conf.js
+++ protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 300000,
+    defaultTimeoutInterval: 120000,
     print: function() {}
   },
   useAllAngular2AppRoots: true,
