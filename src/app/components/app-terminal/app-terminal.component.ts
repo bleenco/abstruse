0ef5871b452b359118511f05a1afdde6fd23dfd5
Izak Lipnik <izak.lipnik@gmail.com>
2017-08-08 14:17:59 +0200

    feat(): collapse/expand commands log in terminal

--- src/app/components/app-terminal/app-terminal.component.ts
+++ src/app/components/app-terminal/app-terminal.component.ts
@@ -1,5 +1,7 @@
-import { Component, ElementRef, OnInit, Input, SimpleChange, EventEmitter } from '@angular/core';
+import {
+  Component, ElementRef, OnInit, Input, SimpleChange, EventEmitter, NgZone } from '@angular/core';
 import { ISlimScrollOptions, SlimScrollEvent } from 'ngx-slimscroll';
+
 import * as AnsiUp from 'ansi_up';
 
 @Component({
@@ -12,8 +14,9 @@ export class AppTerminalComponent implements OnInit {
   au: any;
   scrollOptions: ISlimScrollOptions;
   scrollEvents: EventEmitter<SlimScrollEvent>;
+  commands: { command: string, visible: boolean, output: string }[];
 
-  constructor(private elementRef: ElementRef) {
+  constructor(private elementRef: ElementRef, private ngZone: NgZone) {
     this.scrollOptions = {
       position: 'right',
       barBackground: '#11121A',
@@ -35,6 +38,7 @@ export class AppTerminalComponent implements OnInit {
   ngOnInit() {
     this.au = new AnsiUp.default();
     this.au.use_classes = true;
+    this.commands = [];
   }
 
   ngOnChanges(changes: SimpleChange) {
@@ -42,19 +46,55 @@ export class AppTerminalComponent implements OnInit {
       return;
     }
 
-    const el = this.elementRef.nativeElement.querySelector('.window-terminal-container');
-    if (typeof this.data.clear !== 'undefined') {
-      el.innerHTML = '';
-    } else {
-      el.innerHTML += this.au.ansi_to_html(this.data);
+    this.ngZone.run(() => {
+      const el = this.elementRef.nativeElement.querySelector('.window-terminal-container');
+      if (typeof this.data.clear !== 'undefined') {
+        el.innerHTML = '';
+      } else {
+        let output: string = this.au.ansi_to_html(this.data);
+        if (output) {
+          if (this.commands.length > 0) {
+            if (output.indexOf('==&gt;') !== -1) {
+              let command = output.split('</span>')[0] + '</span>';
+              this.commands.push({
+                command: command,
+                visible: true,
+                output: output
+              });
+            } else {
+              this.commands[this.commands.length - 1].command += ` ${output}`;
+            }
+          } else {
+            let regexp = /<span(.*)==&gt;/gi;
+            regexp.lastIndex = 1;
+            let match = regexp.exec(output);
+            if (match) {
+              let indexEnd = match.index;
+              let indexStart = 0;
+              while (indexEnd >= 0) {
+                let log = output.substring(indexStart, indexEnd);
+                let command = log.split('</span>')[0] + '</span>';
+                this.commands.push({
+                  command: command,
+                  visible: true,
+                  output: log
+                });
+                indexStart = indexEnd;
+                indexEnd = regexp.exec(output).index;
+              }
+            }
+          }
+        }
 
-      const recalculateEvent = new SlimScrollEvent({ type: 'recalculate' });
-      const bottomEvent = new SlimScrollEvent({ type: 'scrollToBottom', duration: 300 });
+        const recalculateEvent = new SlimScrollEvent({ type: 'recalculate' });
+        const bottomEvent = new SlimScrollEvent({ type: 'scrollToBottom', duration: 300 });
 
-      setTimeout(() => {
-        this.scrollEvents.emit(recalculateEvent);
-        this.scrollEvents.emit(bottomEvent);
-      });
-    }
+        setTimeout(() => el.scrollTop = el.scrollHeight);
+      }
+    });
+  }
+
+  toogleCommand(index: number) {
+    this.commands[index].visible = !this.commands[index].visible;
   }
 }
