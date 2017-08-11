a747e694505460420f7060372181c7c5a136e72b
Izak Lipnik <izak.lipnik@gmail.com>
2017-08-11 08:55:37 +0200

    test(): added e2e test for progress bar

--- tests/e2e/webhooks/github/PushEvent.ts
+++ tests/e2e/webhooks/github/PushEvent.ts
@@ -162,3 +162,189 @@ export let header = {
   'X-Hub-Signature': 'sha1=b7c49c4f28d64c153ae1ab7f7a8627a1f66a9e55',
   'X-GitHub-Delivery': '61109660-71f6-11e7-9b49-619c5e386bdf'
 };
+
+
+export let requestD3 = {
+  'ref': 'refs/heads/master',
+  'before': '01e7e2412b73aec952f1afe40e7c714f51a1ecb3',
+  'after': '90e50fa073ef2e7a606207844ffea2e76f0ea7cb',
+  'created': false,
+  'deleted': false,
+  'forced': false,
+  'base_ref': null,
+  'compare': 'https://github.com/Izak88/d3-bundle/compare/01e7e2412b73...90e50fa073ef',
+  'commits': [
+    {
+      'id': '90e50fa073ef2e7a606207844ffea2e76f0ea7cb',
+      'tree_id': 'fefcd12b4f69d01d917abd119d1fd2ae77ad2ba9',
+      'distinct': true,
+      'message': 'test(): do not merge that',
+      'timestamp': '2017-08-10T16:54:43+02:00',
+      'url': 'https://github.com/Izak88/d3-bundle/commit/90e50fa073ef2e7a606207844ffea2e76f0ea7cb',
+      'author': {
+        'name': 'Izak Lipnik',
+        'email': 'izak.lipnik@gmail.com',
+        'username': 'Izak88'
+      },
+      'committer': {
+        'name': 'Izak Lipnik',
+        'email': 'izak.lipnik@gmail.com',
+        'username': 'Izak88'
+      },
+      'added': [
+
+      ],
+      'removed': [
+
+      ],
+      'modified': [
+        'package.json'
+      ]
+    }
+  ],
+  'head_commit': {
+    'id': '90e50fa073ef2e7a606207844ffea2e76f0ea7cb',
+    'tree_id': 'fefcd12b4f69d01d917abd119d1fd2ae77ad2ba9',
+    'distinct': true,
+    'message': 'test(): do not merge that',
+    'timestamp': '2017-08-10T16:54:43+02:00',
+    'url': 'https://github.com/Izak88/d3-bundle/commit/90e50fa073ef2e7a606207844ffea2e76f0ea7cb',
+    'author': {
+      'name': 'Izak Lipnik',
+      'email': 'izak.lipnik@gmail.com',
+      'username': 'Izak88'
+    },
+    'committer': {
+      'name': 'Izak Lipnik',
+      'email': 'izak.lipnik@gmail.com',
+      'username': 'Izak88'
+    },
+    'added': [
+
+    ],
+    'removed': [
+
+    ],
+    'modified': [
+      'package.json'
+    ]
+  },
+  'repository': {
+    'id': 99934351,
+    'name': 'd3-bundle',
+    'full_name': 'Izak88/d3-bundle',
+    'owner': {
+      'name': 'Izak88',
+      'email': 'izak.lipnik@gmail.com',
+      'login': 'Izak88',
+      'id': 8555269,
+      'avatar_url': 'https://avatars0.githubusercontent.com/u/8555269?v=4',
+      'gravatar_id': '',
+      'url': 'https://api.github.com/users/Izak88',
+      'html_url': 'https://github.com/Izak88',
+      'followers_url': 'https://api.github.com/users/Izak88/followers',
+      'following_url': 'https://api.github.com/users/Izak88/following{/other_user}',
+      'gists_url': 'https://api.github.com/users/Izak88/gists{/gist_id}',
+      'starred_url': 'https://api.github.com/users/Izak88/starred{/owner}{/repo}',
+      'subscriptions_url': 'https://api.github.com/users/Izak88/subscriptions',
+      'organizations_url': 'https://api.github.com/users/Izak88/orgs',
+      'repos_url': 'https://api.github.com/users/Izak88/repos',
+      'events_url': 'https://api.github.com/users/Izak88/events{/privacy}',
+      'received_events_url': 'https://api.github.com/users/Izak88/received_events',
+      'type': 'User',
+      'site_admin': false
+    },
+    'private': false,
+    'html_url': 'https://github.com/Izak88/d3-bundle',
+    'description': null,
+    'fork': true,
+    'url': 'https://github.com/Izak88/d3-bundle',
+    'forks_url': 'https://api.github.com/repos/Izak88/d3-bundle/forks',
+    'keys_url': 'https://api.github.com/repos/Izak88/d3-bundle/keys{/key_id}',
+    'collaborators_url':
+      'https://api.github.com/repos/Izak88/d3-bundle/collaborators{/collaborator}',
+    'teams_url': 'https://api.github.com/repos/Izak88/d3-bundle/teams',
+    'hooks_url': 'https://api.github.com/repos/Izak88/d3-bundle/hooks',
+    'issue_events_url': 'https://api.github.com/repos/Izak88/d3-bundle/issues/events{/number}',
+    'events_url': 'https://api.github.com/repos/Izak88/d3-bundle/events',
+    'assignees_url': 'https://api.github.com/repos/Izak88/d3-bundle/assignees{/user}',
+    'branches_url': 'https://api.github.com/repos/Izak88/d3-bundle/branches{/branch}',
+    'tags_url': 'https://api.github.com/repos/Izak88/d3-bundle/tags',
+    'blobs_url': 'https://api.github.com/repos/Izak88/d3-bundle/git/blobs{/sha}',
+    'git_tags_url': 'https://api.github.com/repos/Izak88/d3-bundle/git/tags{/sha}',
+    'git_refs_url': 'https://api.github.com/repos/Izak88/d3-bundle/git/refs{/sha}',
+    'trees_url': 'https://api.github.com/repos/Izak88/d3-bundle/git/trees{/sha}',
+    'statuses_url': 'https://api.github.com/repos/Izak88/d3-bundle/statuses/{sha}',
+    'languages_url': 'https://api.github.com/repos/Izak88/d3-bundle/languages',
+    'stargazers_url': 'https://api.github.com/repos/Izak88/d3-bundle/stargazers',
+    'contributors_url': 'https://api.github.com/repos/Izak88/d3-bundle/contributors',
+    'subscribers_url': 'https://api.github.com/repos/Izak88/d3-bundle/subscribers',
+    'subscription_url': 'https://api.github.com/repos/Izak88/d3-bundle/subscription',
+    'commits_url': 'https://api.github.com/repos/Izak88/d3-bundle/commits{/sha}',
+    'git_commits_url': 'https://api.github.com/repos/Izak88/d3-bundle/git/commits{/sha}',
+    'comments_url': 'https://api.github.com/repos/Izak88/d3-bundle/comments{/number}',
+    'issue_comment_url': 'https://api.github.com/repos/Izak88/d3-bundle/issues/comments{/number}',
+    'contents_url': 'https://api.github.com/repos/Izak88/d3-bundle/contents/{+path}',
+    'compare_url': 'https://api.github.com/repos/Izak88/d3-bundle/compare/{base}...{head}',
+    'merges_url': 'https://api.github.com/repos/Izak88/d3-bundle/merges',
+    'archive_url': 'https://api.github.com/repos/Izak88/d3-bundle/{archive_format}{/ref}',
+    'downloads_url': 'https://api.github.com/repos/Izak88/d3-bundle/downloads',
+    'issues_url': 'https://api.github.com/repos/Izak88/d3-bundle/issues{/number}',
+    'pulls_url': 'https://api.github.com/repos/Izak88/d3-bundle/pulls{/number}',
+    'milestones_url': 'https://api.github.com/repos/Izak88/d3-bundle/milestones{/number}',
+    'notifications_url':
+      'https://api.github.com/repos/Izak88/d3-bundle/notifications{?since,all,participating}',
+    'labels_url': 'https://api.github.com/repos/Izak88/d3-bundle/labels{/name}',
+    'releases_url': 'https://api.github.com/repos/Izak88/d3-bundle/releases{/id}',
+    'deployments_url': 'https://api.github.com/repos/Izak88/d3-bundle/deployments',
+    'created_at': 1502376699,
+    'updated_at': '2017-08-10T14:51:46Z',
+    'pushed_at': 1502376897,
+    'git_url': 'git://github.com/Izak88/d3-bundle.git',
+    'ssh_url': 'git@github.com:Izak88/d3-bundle.git',
+    'clone_url': 'https://github.com/Izak88/d3-bundle.git',
+    'svn_url': 'https://github.com/Izak88/d3-bundle',
+    'homepage': null,
+    'size': 34612,
+    'stargazers_count': 0,
+    'watchers_count': 0,
+    'language': 'JavaScript',
+    'has_issues': false,
+    'has_projects': true,
+    'has_downloads': true,
+    'has_wiki': true,
+    'has_pages': false,
+    'forks_count': 0,
+    'mirror_url': null,
+    'open_issues_count': 0,
+    'forks': 0,
+    'open_issues': 0,
+    'watchers': 0,
+    'default_branch': 'master',
+    'stargazers': 0,
+    'master_branch': 'master'
+  },
+  'pusher': {
+    'name': 'Izak88',
+    'email': 'izak.lipnik@gmail.com'
+  },
+  'sender': {
+    'login': 'Izak88',
+    'id': 8555269,
+    'avatar_url': 'https://avatars0.githubusercontent.com/u/8555269?v=4',
+    'gravatar_id': '',
+    'url': 'https://api.github.com/users/Izak88',
+    'html_url': 'https://github.com/Izak88',
+    'followers_url': 'https://api.github.com/users/Izak88/followers',
+    'following_url': 'https://api.github.com/users/Izak88/following{/other_user}',
+    'gists_url': 'https://api.github.com/users/Izak88/gists{/gist_id}',
+    'starred_url': 'https://api.github.com/users/Izak88/starred{/owner}{/repo}',
+    'subscriptions_url': 'https://api.github.com/users/Izak88/subscriptions',
+    'organizations_url': 'https://api.github.com/users/Izak88/orgs',
+    'repos_url': 'https://api.github.com/users/Izak88/repos',
+    'events_url': 'https://api.github.com/users/Izak88/events{/privacy}',
+    'received_events_url': 'https://api.github.com/users/Izak88/received_events',
+    'type': 'User',
+    'site_admin': false
+  }
+};
