160248ed9a08f95eb04b3df8b347b736f315cefc
Jan Kuri <jkuri88@gmail.com>
2017-08-20 06:41:18 +0200

    feat(sockets): full-screen socket connection status if disconnected / retrying to server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocketService } from './services/socket.service';
+import { ConnectionStates } from './classes/rx-web-socket.class';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+  connected: boolean;
+  state: ConnectionStates;
+
+  constructor(private socketService: SocketService) { }
+
+  ngOnInit() {
+    this.socketService.connectionState
+      .subscribe(state => {
+        this.state = state;
+
+        if (state === ConnectionStates.CONNECTED) {
+          this.connected = true;
+        } else {
+          this.connected = false;
+        }
+      });
+  }
+}
