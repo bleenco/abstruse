6a222538d2402627641f9f843d2ed911d2dd67f6
Izak Lipnik <izak.lipnik@gmail.com>
2017-09-04 11:55:06 +0200

    test(): added e2e protractor test for environment variables

--- e2e/040_repositories.e2e.ts
+++ e2e/040_repositories.e2e.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ExpectedConditions } from 'protractor';
 import { isLoaded, login, logout } from './utils';
 import { request, header } from '../tests/e2e/webhooks/github/PingEvent';
 import { sendGitHubRequest } from '../tests/e2e/utils/utils';
@@ -25,4 +25,28 @@ describe('Repositories', () => {
       .then(() => expect(element(by.css('.bold')).getText()).toContain('bterm'));
   });
 
+  it('should redirect to bterm repository, add new environment variable and then delete it', () => {
+    return  browser.get('/repositories')
+      .then((): any => browser.wait(() => {
+        return element.all(by.css('.list-item')).count().then(count => count === 1);
+      }))
+      .then((): any => element.all(by.css('.list-item')).first().click())
+      .then((): any => browser.getCurrentUrl())
+      .then(url => expect(url).toEqual('http://localhost:6500/repo/1'))
+      .then((): any => browser.wait(() => element(by.css(`[name="btn-settings"]`)).isPresent()))
+      .then((): any => browser.wait(() => {
+        return ExpectedConditions.elementToBeClickable(element(by.css(`[name="btn-settings"]`)));
+      }))
+      .then((): any => element(by.css('[name="btn-settings"]')).click())
+      .then(() => element(by.css('.form-input[name="name"]')).sendKeys('test'))
+      .then(() => element(by.css('.form-input[name="value"]')).sendKeys('test'))
+      .then(() => element(by.css('[name="btn-add-variable"]')).click())
+      .then((): any => browser.wait(() => {
+        return element.all(by.css('.list-item-slim')).count().then(count => count === 1);
+      }))
+      .then(() => element(by.css('[name="remove-variable"]')).click())
+      .then((): any => browser.wait(() => {
+        return element.all(by.css('.list-item-slim')).count().then(count => count === 0);
+      }));
+  });
 });
