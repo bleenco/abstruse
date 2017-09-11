6819c791de981a95c1eebb29e5907c260084ecd9
Jan Kuri <jkuri88@gmail.com>
2017-09-11 08:27:19 +0200

    build(ci): overlay2 fs for docker inside docker

--- tests/postinstall_ci.js
+++ tests/postinstall_ci.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 const filePath = join('/etc/docker/daemon.json');
 const obj = {
-  "storage-driver": "overlay2"
+  "storage-driver": "aufs"
 };
 
 mkdirSync(join('/etc/docker'));
