9760d1e516132cad0d7bfc7b320dd3b4fb66e68b
Izak Lipnik <izak.lipnik@gmail.com>
2017-10-02 08:44:19 +0200

    test(): updated webhooks for gitlab repository

--- tests/e2e/webhooks/gitlab/MergeEvents.ts
+++ tests/e2e/webhooks/gitlab/MergeEvents.ts
@@ -3,40 +3,40 @@ export let request = {
   'user': {
     'name': 'Izak Lipnik',
     'username': 'izak88',
-    'avatar_url': 'https://secure.gravatar.com/avatar/'
-                  + 'c27a4cf56134b3956555fc8e1d7cced0?s=80&d=identicon'
+    'avatar_url':
+      'https://secure.gravatar.com/avatar/c27a4cf56134b3956555fc8e1d7cced0?s=80&d=identicon'
   },
   'project': {
-    'name': 'test',
-    'description': 'test',
-    'web_url': 'https://gitlab.com/izak88/test',
+    'name': 'd3-bundle',
+    'description': '',
+    'web_url': 'https://gitlab.com/izak88/d3-bundle',
     'avatar_url': null,
-    'git_ssh_url': 'git@gitlab.com:izak88/test.git',
-    'git_http_url': 'https://gitlab.com/izak88/test.git',
+    'git_ssh_url': 'git@gitlab.com:izak88/d3-bundle.git',
+    'git_http_url': 'https://gitlab.com/izak88/d3-bundle.git',
     'namespace': 'izak88',
     'visibility_level': 20,
-    'path_with_namespace': 'izak88/test',
+    'path_with_namespace': 'izak88/d3-bundle',
     'default_branch': 'master',
     'ci_config_path': null,
-    'homepage': 'https://gitlab.com/izak88/test',
-    'url': 'git@gitlab.com:izak88/test.git',
-    'ssh_url': 'git@gitlab.com:izak88/test.git',
-    'http_url': 'https://gitlab.com/izak88/test.git'
+    'homepage': 'https://gitlab.com/izak88/d3-bundle',
+    'url': 'git@gitlab.com:izak88/d3-bundle.git',
+    'ssh_url': 'git@gitlab.com:izak88/d3-bundle.git',
+    'http_url': 'https://gitlab.com/izak88/d3-bundle.git'
   },
   'object_attributes': {
-    'id': 4770446,
+    'id': 5244277,
     'target_branch': 'master',
-    'source_branch': 'testbranch',
-    'source_project_id': 3827788,
+    'source_branch': 'test',
+    'source_project_id': 4283451,
     'author_id': 1505971,
     'assignee_id': null,
-    'title': 'test',
-    'created_at': '2017-08-22 14:23:41 UTC',
-    'updated_at': '2017-08-22 14:23:41 UTC',
+    'title': 'test branch',
+    'created_at': '2017-10-02 06:39:59 UTC',
+    'updated_at': '2017-10-02 06:39:59 UTC',
     'milestone_id': null,
     'state': 'opened',
     'merge_status': 'unchecked',
-    'target_project_id': 3827788,
+    'target_project_id': 4283451,
     'iid': 1,
     'description': '',
     'updated_by_id': null,
@@ -60,44 +60,44 @@ export let request = {
     'ref_fetched': true,
     'merge_jid': null,
     'source': {
-      'name': 'test',
-      'description': 'test',
-      'web_url': 'https://gitlab.com/izak88/test',
+      'name': 'd3-bundle',
+      'description': '',
+      'web_url': 'https://gitlab.com/izak88/d3-bundle',
       'avatar_url': null,
-      'git_ssh_url': 'git@gitlab.com:izak88/test.git',
-      'git_http_url': 'https://gitlab.com/izak88/test.git',
+      'git_ssh_url': 'git@gitlab.com:izak88/d3-bundle.git',
+      'git_http_url': 'https://gitlab.com/izak88/d3-bundle.git',
       'namespace': 'izak88',
       'visibility_level': 20,
-      'path_with_namespace': 'izak88/test',
+      'path_with_namespace': 'izak88/d3-bundle',
       'default_branch': 'master',
       'ci_config_path': null,
-      'homepage': 'https://gitlab.com/izak88/test',
-      'url': 'git@gitlab.com:izak88/test.git',
-      'ssh_url': 'git@gitlab.com:izak88/test.git',
-      'http_url': 'https://gitlab.com/izak88/test.git'
+      'homepage': 'https://gitlab.com/izak88/d3-bundle',
+      'url': 'git@gitlab.com:izak88/d3-bundle.git',
+      'ssh_url': 'git@gitlab.com:izak88/d3-bundle.git',
+      'http_url': 'https://gitlab.com/izak88/d3-bundle.git'
     },
     'target': {
-      'name': 'test',
-      'description': 'test',
-      'web_url': 'https://gitlab.com/izak88/test',
+      'name': 'd3-bundle',
+      'description': '',
+      'web_url': 'https://gitlab.com/izak88/d3-bundle',
       'avatar_url': null,
-      'git_ssh_url': 'git@gitlab.com:izak88/test.git',
-      'git_http_url': 'https://gitlab.com/izak88/test.git',
+      'git_ssh_url': 'git@gitlab.com:izak88/d3-bundle.git',
+      'git_http_url': 'https://gitlab.com/izak88/d3-bundle.git',
       'namespace': 'izak88',
       'visibility_level': 20,
-      'path_with_namespace': 'izak88/test',
+      'path_with_namespace': 'izak88/d3-bundle',
       'default_branch': 'master',
       'ci_config_path': null,
-      'homepage': 'https://gitlab.com/izak88/test',
-      'url': 'git@gitlab.com:izak88/test.git',
-      'ssh_url': 'git@gitlab.com:izak88/test.git',
-      'http_url': 'https://gitlab.com/izak88/test.git'
+      'homepage': 'https://gitlab.com/izak88/d3-bundle',
+      'url': 'git@gitlab.com:izak88/d3-bundle.git',
+      'ssh_url': 'git@gitlab.com:izak88/d3-bundle.git',
+      'http_url': 'https://gitlab.com/izak88/d3-bundle.git'
     },
     'last_commit': {
-      'id': '56cbb9d102695e5f8a1dbd47a23e99a4b425dc30',
-      'message': 'test\n',
-      'timestamp': '2017-08-22T16:22:20+02:00',
-      'url': 'https://gitlab.com/izak88/test/commit/56cbb9d102695e5f8a1dbd47a23e99a4b425dc30',
+      'id': '9ca1197fe5a5c0f2567fb0c9fdb0d5f8c9c919d9',
+      'message': 'test branch\n',
+      'timestamp': '2017-10-02T08:39:29+02:00',
+      'url': 'https://gitlab.com/izak88/d3-bundle/commit/9ca1197fe5a5c0f2567fb0c9fdb0d5f8c9c919d9',
       'author': {
         'name': 'Izak Lipnik',
         'email': 'izak.lipnik@gmail.com'
@@ -107,17 +107,17 @@ export let request = {
     'total_time_spent': 0,
     'human_total_time_spent': null,
     'human_time_estimate': null,
-    'url': 'https://gitlab.com/izak88/test/merge_requests/1',
+    'url': 'https://gitlab.com/izak88/d3-bundle/merge_requests/1',
     'action': 'open'
   },
   'labels': [
 
   ],
   'repository': {
-    'name': 'test',
-    'url': 'git@gitlab.com:izak88/test.git',
-    'description': 'test',
-    'homepage': 'https://gitlab.com/izak88/test'
+    'name': 'd3-bundle',
+    'url': 'git@gitlab.com:izak88/d3-bundle.git',
+    'description': '',
+    'homepage': 'https://gitlab.com/izak88/d3-bundle'
   }
 };
 
