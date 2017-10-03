ad127aaebc08fa482f808420fc1a91338940f442
Izak Lipnik <izak.lipnik@gmail.com>
2017-10-03 09:17:15 +0200

    test(): added e2e tests for api routes

---
+++ tests/e2e/tests/050-routes/030-user/020-api-user-login.ts
@@ -0,0 +1,29 @@
+import * as request from 'request';
+
+export default function() {
+  return Promise.resolve()
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        let options = {
+          url: 'http://localhost:6500/api/user/login',
+          method: 'POST',
+          json: { test: 1 }
+        };
+
+        request(options, (err, response, body) => {
+          if (err) {
+            Promise.reject(err);
+          } else {
+            if (response.statusCode === 200) {
+              resolve(body);
+            } else {
+              reject({
+                statusCode: response.statusCode,
+                response: body
+              });
+            }
+          }
+        });
+      });
+    });
+}
