9d696042fb15faa5e1544e0afc1258b2518fbe90
Jan Kuri <jkuri88@gmail.com>
2017-10-04 13:25:22 +0200

    test(e2e): socket test sample for authorized request and not authorized request

--- tests/unit/040_socket.spec.ts
+++ tests/unit/040_socket.spec.ts
@@ -4,6 +4,7 @@ import { join } from 'path';
 import * as temp from 'temp';
 import { execSilent, exec, abstruse, killAllProcesses } from '../e2e/utils/process';
 import * as ws from 'ws';
+import { sendRequest } from '../helpers/utils';
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
@@ -41,6 +42,45 @@ describe('Socket Security', () => {
       expect(data.data).to.not.be.a('null');
       done();
     });
+  });
+
+  it(`should not have permissions to trigger build image on 'buildImage' event`, (done) => {
+    socket = new ws('ws://localhost:6501', null);
+
+    socket.on('message', (data: any) => {
+      data = JSON.parse(data);
+
+      if (data.type === 'error') {
+        expect(data.data).to.equal('not authorized');
+        done();
+      }
+    });
+
+    socket.on('open', () => socket.send(JSON.stringify({ type: 'buildImage', data: null })));
+  });
+
+  it(`should have permissions to trigger build image on 'buildImage' event`, (done) => {
+    let registerData = {
+      email: 'test@gmail.com', fullname: 'test',
+      password: 'test', confirmPassword: 'test', admin: 1
+    };
+    let loginData = { email: 'test@gmail.com', password: 'test' };
+
+    sendRequest(registerData, 'api/user/create')
+      .then(() => sendRequest(loginData, 'api/user/login'))
+      .then((jwt: any) => {
+        socket = new ws('ws://localhost:6501', jwt.data);
+
+        socket.on('message', (data: any) => {
+          data = JSON.parse(data);
+          if (data.type === 'request_received') {
+            done();
+          }
+        });
+
+        socket.on('open', () => socket.send(JSON.stringify({ type: 'buildImage', data: null })));
+      })
+      .catch(err => console.error(err));
   });
 
 });
