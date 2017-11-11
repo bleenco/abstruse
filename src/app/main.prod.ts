be5879af2031e9fe2b8071999affe9e319d03b9f
Jan Kuri <jkuri88@gmail.com>
2017-11-11 10:19:35 +0100

    build(webpack): configure AngulaerCompilerPlugin properly

--- src/app/main.prod.ts
+++ src/app/main.prod.ts
@@ -1,4 +1,6 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
