66537466721b98fe5fe0f31745cd1f108bf5df3f
Izak Lipnik <izak.lipnik@gmail.com>
2017-11-24 16:59:39 +0100

    feat(deploy): support for deploy to aws s3

---
+++ src/api/deploy.ts
@@ -0,0 +1,48 @@
+import { Observable, Observer } from 'rxjs';
+import { s3Deploy } from './deploy/aws-s3';
+import { codeDeploy } from './deploy/aws-code-deploy';
+
+export function deploy(preferences: any, container: string, variables: string[]): Observable<any> {
+  return new Observable((observer: Observer<any>) => {
+    if (preferences) {
+      const provider = preferences.provider;
+      deployProvider(provider, preferences, container, variables).subscribe(event => {
+        observer.next(event);
+      },
+      err => observer.error(err),
+      () => observer.complete());
+    } else {
+      observer.complete();
+    }
+  });
+}
+
+function deployProvider(provider, preferences, container, variables): Observable<any> {
+  switch (provider) {
+    case 's3':
+      return s3Deploy(preferences, container, variables);
+    case 'codeDeploy':
+      return codeDeploy(preferences, container, variables);
+    default:
+      return new Observable((observer: Observer<any>) => {
+        observer.error({
+          type: 'containerError',
+          data: `Deployment provider ${provider} is not supported.`
+        });
+        observer.complete();
+      });
+  }
+}
+
+export function findFromEnvVariables(variables, property) {
+  let value = variables.find(v => v.startsWith(property));
+
+  if (value) {
+    const tmp = value.split('=');
+    if (tmp.length > 1) {
+      return tmp[1];
+    }
+  }
+
+  return null;
+}
