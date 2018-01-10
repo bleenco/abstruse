f5796af063bbe5a66b05665d0112b132b1242c91
Jan Kuri <jkuri88@gmail.com>
2018-01-10 20:05:55 +0100

    build(webpack): add purify plugin

--- webpack.api.js
+++ webpack.api.js
@@ -23,7 +23,18 @@ module.exports = {
       { test: /\.ts$/, loaders: ['ts-loader?silent=true'] },
     ]
   },
-  stats: 'errors-only',
+  stats: { 
+    assets: false,
+    chunks: false,
+    chunkModules: false,
+    colors: true,
+    timings: true,
+    children: false,
+    cachedAssets: false,
+    chunkOrigins: false,
+    modules: false,
+    warnings: false
+  },
   node: {
     console: false,
     global: false,
