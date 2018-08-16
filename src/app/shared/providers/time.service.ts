928ed6ef2783b0513c17f27c894b6513e1e0dbdb
Jan Kuri <jkuri88@gmail.com>
2018-08-16 13:47:26 +0200

    feat(ui): time service provider

---
+++ src/app/shared/providers/time.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, Provider } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { share } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimeService {
+  timer: Observable<any>;
+
+  constructor() {
+    this.timer = timer(1000, 1000);
+  }
+
+  getCurrentTime(): Observable<any> {
+    return new Observable(observer => {
+      const sub = this.timer.subscribe(() => {
+        observer.next(new Date().getTime());
+      });
+
+      return () => {
+        sub.unsubscribe();
+      };
+    }).pipe(share());
+  }
+}
