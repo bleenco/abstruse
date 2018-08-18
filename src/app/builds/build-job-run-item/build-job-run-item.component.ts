a909e998c26f76178caf74dc864e25cb7e54b37b
Jan Kuri <jkuri88@gmail.com>
2018-08-18 06:34:54 +0200

    fix(ui): fix running time on job details

--- src/app/builds/build-job-run-item/build-job-run-item.component.ts
+++ src/app/builds/build-job-run-item/build-job-run-item.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-build-job-run-item',
   templateUrl: './build-job-run-item.component.html',
   styleUrls: ['./build-job-run-item.component.sass']
 })
-export class BuildJobRunItemComponent implements OnInit {
+export class BuildJobRunItemComponent implements OnInit, OnChanges {
   @Input() run: any;
   @Input() num: number;
 
@@ -14,6 +14,18 @@ export class BuildJobRunItemComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.runningTime = Math.abs(this.run.end_time - this.run.start_time);
+    this.calculateTime();
+  }
+
+  ngOnChanges() {
+    this.calculateTime();
+  }
+
+  private calculateTime(): void {
+    if (!this.run.end_time) {
+      this.runningTime = null;
+    } else {
+      this.runningTime = Math.abs(this.run.end_time - this.run.start_time);
+    }
   }
 }
