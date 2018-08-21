a9f89ddf54a2884c26f7b42c52eebd09e57e0e6a
Jan Kuri <jkuri88@gmail.com>
2018-08-21 02:49:58 +0200

    feat(header): init routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,59 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 import { AuthGuardService } from './shared/providers/auth-guard.service';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'builds' },
-  { path: 'login', redirectTo: '' },
-  { path: 'builds', loadChildren: './builds/builds.module#BuildsModule', canLoad: [AuthGuardService], canActivate: [AuthGuardService] },
-  { path: 'images', loadChildren: './images/images.module#ImagesModule', canLoad: [AuthGuardService], canActivate: [AuthGuardService] },
-  { path: 'setup', loadChildren: './setup/setup.module#SetupModule' },
-  { path: '404', component: NotFoundComponent }
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'builds'
+  },
+  {
+    path: 'login',
+    redirectTo: ''
+  },
+  {
+    path: 'builds',
+    loadChildren: './builds/builds.module#BuildsModule',
+    canLoad: [AuthGuardService],
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'dashboard',
+    loadChildren: './dashboard/dashboard.module#DashboardModule',
+    canLoad: [AuthGuardService],
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'repositories',
+    loadChildren: './repositories/repositories.module#RepositoriesModule',
+    canLoad: [AuthGuardService],
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'team',
+    loadChildren: './team/team.module#TeamModule',
+    canLoad: [AuthGuardService],
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'images',
+    loadChildren: './images/images.module#ImagesModule',
+    canLoad: [AuthGuardService],
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'settings',
+    loadChildren: './settings/settings.module#SettingsModule',
+    canLoad: [AuthGuardService],
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'setup',
+    loadChildren: './setup/setup.module#SetupModule'
+  },
+  {
+    path: '404',
+    component: NotFoundComponent
+  }
 ];
 
 @NgModule({
