fd551e7ed2ebeadbd53f2e3c54ba0f558900b370
Jan Kuri <jkuri88@gmail.com>
2019-01-05 11:43:16 +0100

    cleanup(): remove commented code from worker

--- cmd/abstruse-worker/worker.go
+++ cmd/abstruse-worker/worker.go
@@ -27,33 +27,6 @@ func main() {
 		fatal(err)
 	}
 
-	// url := "https://github.com/jkuri/d3-bundle.git"
-
-	// if err := git.TestClone(url); err != nil {
-	// 	fmt.Printf("%+v", err)
-	// 	os.Exit(1)
-	// }
-
-	// contents, err := git.FetchAbstruseConfig(url)
-	// if err != nil {
-	// 	fmt.Printf("%+v", err)
-	// 	os.Exit(1)
-	// }
-
-	// fmt.Println(contents)
-
-	// if err := worker.RunContainer(); err != nil {
-	// 	fatal(err)
-	// }
-
-	// file := "/Users/jan/Documents/abstruse.sqlite"
-	// stat, err := client.UploadFile(context.Background(), file)
-	// if err != nil {
-	// 	fatal(err)
-	// }
-
-	// fmt.Printf("%s\n", humanize.Bytes(uint64(stat.BytesSent)))
-
 	os.Exit(0)
 }
 
