d93e2fe42b3d4ac0acd68d6217b569b7609cb3ee
Jan Kuri <jkuri88@gmail.com>
2019-08-26 23:15:04 +0200

    feat(db): create database if not exists and set backoff for connection retry

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/eapache/channels v1.1.0
 	github.com/fatih/color v1.7.0
 	github.com/felixge/httpsnoop v1.0.0
+	github.com/go-sql-driver/mysql v1.4.1
 	github.com/gobwas/httphead v0.0.0-20180130184737-2c6c146eadee
 	github.com/gobwas/pool v0.2.0 // indirect
 	github.com/gobwas/ws v1.0.2
--- server/db/connection.go
+++ server/db/connection.go
@@ -1,6 +1,12 @@
 package db
 
 import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/cenkalti/backoff"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql" // MySQL driver
 )
@@ -19,16 +25,55 @@ type Options struct {
 	Charset  string
 }
 
+func (o *Options) check() error {
+	conn, err := sql.Open("mysql", connStr(*o, false))
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	if err := conn.Ping(); err != nil {
+		return fmt.Errorf("mysql: could not establish a connection")
+	}
+
+	if _, err := conn.Exec("USE " + o.Database); err != nil {
+		if merr, ok := err.(*mysql.MySQLError); ok && merr.Number == 1049 {
+			if _, err := conn.Exec("CREATE DATABASE IF NOT EXISTS " + o.Database); err != nil {
+				return fmt.Errorf("mysql: could not create database: %s", err.Error())
+			}
+		}
+	}
+
+	return nil
+}
+
 // Connect initializes database connection.
 func Connect(opts Options) error {
-	connstr := opts.User + ":" + opts.Password + "@tcp(" + opts.Hostname + ":" + opts.Port + ")/" + opts.Database + "?charset=" + opts.Charset + "&parseTime=true&loc=Local"
-	conn, err := gorm.Open("mysql", connstr)
+	// connstr := opts.User + ":" + opts.Password + "@tcp(" + opts.Hostname + ":" + opts.Port + ")/" + opts.Database + "?charset=" + opts.Charset + "&parseTime=true&loc=Local"
+	var err error
+	ticker := backoff.NewTicker(backoff.NewConstantBackOff(10*time.Second))
+
+	for range ticker.C {
+		if err = opts.check(); err != nil {
+			fmt.Printf("database error: %s, retrying in 10s...\n", err.Error())
+			continue
+		}
+
+		ticker.Stop()
+		break
+	}
+
+	if err != nil {
+		return err
+	}
+
+	conn, err := gorm.Open("mysql", connStr(opts, true))
 	if err != nil {
 		return err
 	}
 	DB = conn
 
-	DB.AutoMigrate(
+	conn.AutoMigrate(
 		&User{},
 		&Team{},
 		&Permission{},
@@ -39,7 +84,22 @@ func Connect(opts Options) error {
 		&Job{},
 	)
 
-	initDefaultValues()
-
 	return nil
+}
+
+func connStr(opts Options, useDb bool) string {
+	var cred string
+	if opts.User != "" {
+		cred = opts.User
+		if opts.Password != "" {
+			cred = cred + ":" + opts.Password
+		}
+		cred = cred + "@"
+	}
+
+	if useDb {
+		return fmt.Sprintf("%stcp([%s]:%s)/%s?charset=%s&parseTime=true&loc=Local", cred, opts.Hostname, opts.Port, opts.Database, opts.Charset)
+	}
+
+	return fmt.Sprintf("%stcp([%s]:%s)/", cred, opts.Hostname, opts.Port)
 }
