d284557208eae9dca7b5a625dd2e18d3ac688612
Jan Kuri <jkuri88@gmail.com>
2020-05-14 18:02:51 +0200

    feat(ui): websocket model

---
+++ web/abstruse/src/app/core/shared/shared-functions.ts
@@ -0,0 +1,48 @@
+import { Observable, of } from 'rxjs';
+
+export function throwIfAlreadyLoaded(parentModule: any, moduleName: string) {
+  if (parentModule) {
+    throw new Error(`${moduleName} has already been loaded. Import Core modules in the AppModule only.`);
+  }
+}
+
+export function handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+    console.error(error);
+    return of(result as T);
+  };
+}
+
+export function getURL(): string {
+  const secure = location.protocol === 'https:' ? true : false;
+  const port = location.port === '4200' || location.port === '6500' ? '6500' : '';
+
+  return secure ? `https://${location.hostname}:${port}` : `http://${location.hostname}:${port}`;
+}
+
+export function getAPIURL(): string {
+  return `/api`;
+}
+
+export function getWebSocketURL(): string {
+  const secure = location.protocol === 'https:' ? true : false;
+  const port = location.port === '80' ? '' : `:${location.port}`;
+
+  if (secure) {
+    return `wss://${location.hostname}${port}/ws`;
+  } else {
+    return `ws://${location.hostname}${port}/ws`;
+  }
+}
+
+export function getAvatars(): string[] {
+  return Array.from(new Array(30), (x, i) => `/avatars/predefined/avatar_${i + 1}.svg`);
+}
+
+export function hexToRGB(hex: string, alpha: number = 1) {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  return `rgba(${r},${g},${b},${alpha})`;
+}
