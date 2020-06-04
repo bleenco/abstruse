0f98dbed02d71e7729eba35e724530059f3a7171
Jan Kuri <jkuri88@gmail.com>
2020-06-04 11:17:41 +0200

    feat(ui): terminal styles

--- web/abstruse/src/app/shared/widgets/terminal/terminal.component.ts
+++ web/abstruse/src/app/shared/widgets/terminal/terminal.component.ts
@@ -1,35 +1,59 @@
-import { Component, OnInit, OnDestroy, Input, ElementRef, OnChanges } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, ElementRef, OnChanges, ViewEncapsulation } from '@angular/core';
 import { ITheme, Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
 
 @Component({
   selector: 'app-terminal',
   templateUrl: './terminal.component.html',
-  styleUrls: ['./terminal.component.sass']
+  styleUrls: ['./terminal.component.sass'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TerminalComponent implements OnInit, OnDestroy, OnChanges {
   @Input() data: string;
 
   terminal: Terminal;
   fitAddon: FitAddon;
+  themeLight: ITheme = {
+    foreground: '#615f51',
+    background: '#fff',
+    black: '#050505',
+    red: '#b0263f',
+    green: '#4b862c',
+    yellow: '#a18912',
+    blue: '#3b62d9',
+    magenta: '#a431c4',
+    cyan: '#178262',
+    white: '#fbf1bc',
+    brightBlack: '#0e0e0c',
+    brightRed: '#b72424',
+    brightGreen: '#4b862c',
+    brightYellow: '#87400d',
+    brightBlue: '#3b62d9',
+    brightMagenta: '#a431c4',
+    brightCyan: '#178262',
+    brightWhite: '#fbf1bc',
+    cursor: 'rgba(0, 0, 0, 0)',
+    cursorAccent: 'rgba(0, 0, 0, 0)',
+    selection: 'rgba(0, 0, 0, 0)'
+  };
   theme: ITheme = {
-    foreground: '#F8F8F2',
-    background: '#3E3F42',
+    foreground: 'hsl(220, 14%, 71%)',
+    background: 'hsl(220, 13%, 18%)',
     black: '#000',
-    red: '#ff5555',
-    green: '#50fa7b',
-    yellow: '#f1fa8c',
-    blue: '#96ECFD',
-    magenta: '#bd93f9',
-    cyan: '#8be9fd',
+    red: 'hsl(355, 65%, 65%)',
+    green: 'hsl( 95, 38%, 62%)',
+    yellow: 'hsl( 29, 54%, 61%)',
+    blue: 'hsl(207, 82%, 66%)',
+    magenta: 'hsl(286, 60%, 67%)',
+    cyan: 'hsl(187, 47%, 55%)',
     white: '#ffffff',
     brightBlack: '#666666',
-    brightRed: '#ff5555',
-    brightGreen: '#50fa7b',
-    brightYellow: '#f1fa8c',
-    brightBlue: '#96ECFD',
-    brightMagenta: '#bd93f9',
-    brightCyan: '#8be9fd',
+    brightRed: 'hsl(5, 48%, 51%)',
+    brightGreen: 'hsl( 95, 38%, 62%)',
+    brightYellow: 'hsl( 39, 67%, 69%)',
+    brightBlue: 'hsl(207, 82%, 66%)',
+    brightMagenta: 'hsl(286, 60%, 67%)',
+    brightCyan: 'hsl(187, 47%, 55%)',
     brightWhite: '#ffffff',
     cursor: 'rgba(0, 0, 0, 0)',
     cursorAccent: 'rgba(0, 0, 0, 0)',
@@ -42,7 +66,7 @@ export class TerminalComponent implements OnInit, OnDestroy, OnChanges {
       disableStdin: true,
       scrollback: 10000,
       drawBoldTextInBrightColors: true,
-      fontWeightBold: '500'
+      fontWeightBold: '700'
     });
     this.fitAddon = new FitAddon();
     this.terminal.loadAddon(this.fitAddon);
