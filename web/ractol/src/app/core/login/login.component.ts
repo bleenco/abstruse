79fa564fd822f1eb9a8631167495cafd286990f8
Jan Kuri <jkuri88@gmail.com>
2020-07-03 13:15:53 +0200

    feat(ui): login form

--- web/ractol/src/app/core/login/login.component.ts
+++ web/ractol/src/app/core/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Login } from './login.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+  form: Login = { email: '', password: '' };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ web/ractol/src/app/core/login/login.model.ts
@@ -0,0 +1,4 @@
+export interface Login {
+  email: string;
+  password: string;
+}
