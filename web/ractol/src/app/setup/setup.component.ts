72fed77ce7f4633721df209d35e42f59e9d33746
Jan Kuri <jkuri88@gmail.com>
2020-07-08 18:56:34 +0200

    fix(setup): set step based on route in case browser refresh

--- web/ractol/src/app/setup/setup.component.ts
+++ web/ractol/src/app/setup/setup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProgressWizardOptions } from '../shared';
 import { SetupWizard } from './shared/wizard.model';
 import { SetupService } from './shared/setup.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-setup',
@@ -11,10 +12,14 @@ export class SetupComponent implements OnInit {
   progressOptions: ProgressWizardOptions;
   wizard: SetupWizard;
 
-  constructor(private setup: SetupService) {
+  constructor(private setup: SetupService, private router: Router) {
     this.wizard = this.setup.wizard;
     this.progressOptions = { steps: this.setup.wizard.steps.map(s => s.route) };
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const splitted = this.router.url.split('/');
+    const step = this.wizard.steps.find(s => s.route === splitted[splitted.length - 1]);
+    this.setup.wizard.step = step!.step;
+  }
 }
