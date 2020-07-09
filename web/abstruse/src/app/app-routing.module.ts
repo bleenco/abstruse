de95f740c0406d14c6d8ba3bb353b1e1b7c9031c
Jan Kuri <jkuri88@gmail.com>
2020-07-09 02:20:17 +0200

    chore(ui): preload all modules

--- web/abstruse/src/app/app-routing.module.ts
+++ web/abstruse/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { AuthGuardService } from './auth/shared/auth-guard.service';
 import { SetupGuardService } from './setup/shared/setup-guard.service';
@@ -26,7 +26,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
