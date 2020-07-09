ad32e7b945b44d37e9d65528ca942d398df80d88
Jan Kuri <jkuri88@gmail.com>
2020-07-09 02:15:30 +0200

    build(ui): lazy-load setup route

--- web/abstruse/src/app/app.module.ts
+++ web/abstruse/src/app/app.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from './core';
 import { SharedModule } from './shared';
 import { AuthModule } from './auth/auth.module';
-import { SetupModule } from './setup/setup.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BuildsModule } from './builds/builds.module';
 
 @NgModule({
-  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule.forRoot(), AuthModule, SetupModule, BuildsModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule.forRoot(), AuthModule, BuildsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
