ba5e95735b8b7261bcd937e8653021bab87915b2
Jan Kuri <jkuri88@gmail.com>
2020-07-13 15:31:05 +0200

    feat(cleanup): deprecated providers list and related items

--- web/abstruse/src/app/providers/providers.module.ts
+++ web/abstruse/src/app/providers/providers.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProvidersRoutingModule } from './providers-routing.module';
-import { ProvidersListComponent } from './providers-list/providers-list.component';
-import { ProvidersModalComponent } from './providers-modal/providers-modal.component';
 import { SharedModule } from '../shared';
-import { ProvidersListItemComponent } from './providers-list-item/providers-list-item.component';
-import { ProvidersReposListComponent } from './providers-repos-list/providers-repos-list.component';
-import { ProvidersReposListItemComponent } from './providers-repos-list-item/providers-repos-list-item.component';
 import { ProvidersComponent } from './providers/providers.component';
 import { ProviderItemComponent } from './provider-item/provider-item.component';
+import { ProvidersModalComponent } from './providers-modal/providers-modal.component';
 
 @NgModule({
-  declarations: [ProvidersListComponent, ProvidersModalComponent, ProvidersListItemComponent, ProvidersReposListComponent, ProvidersReposListItemComponent, ProvidersComponent, ProviderItemComponent],
+  declarations: [ProvidersModalComponent, ProvidersComponent, ProviderItemComponent],
   imports: [CommonModule, ProvidersRoutingModule, SharedModule],
   entryComponents: [ProvidersModalComponent]
 })
