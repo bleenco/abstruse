488857d5b490877e4a944889b0a21b3f061947ad
Jan Kuri <jkuri88@gmail.com>
2020-07-16 17:35:40 +0200

    feat(ui): workers module

---
+++ web/abstruse/src/app/workers/workers.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WorkersRoutingModule } from './workers-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WorkersRoutingModule
+  ]
+})
+export class WorkersModule { }
