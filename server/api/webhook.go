7a00bd4f1f05e83d0deeb7e436c6005434b428b8
Jan Kuri <jkuri88@gmail.com>
2020-07-30 07:52:23 +0200

    test(github): add github hooks for testing purposes

--- server/api/webhook.go
+++ server/api/webhook.go
@@ -68,6 +68,7 @@ func (h *webhooks) hook() http.HandlerFunc {
 	// })
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
 		render.JSON(w, http.StatusOK, render.Empty{})
 	})
 }
