ab23f9ee3da1b106ddb0d2ddbd6f0903fd218dda
Jan Kuri <jkuri88@gmail.com>
2020-12-05 15:02:19 +0100

    chore(web): indent json files

--- web/abstruse/karma.conf.js
+++ web/abstruse/karma.conf.js
@@ -21,10 +21,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/abstruse'),
       subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
