69874d002878eb2440437b22c61af05798ad624f
Jan Kuri <jkuri88@gmail.com>
2020-12-15 14:51:09 +0100

    fix(web): fix logo url query params

--- web/abstruse/src/app/builds/index/index.component.ts
+++ web/abstruse/src/app/builds/index/index.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { BuildsItemsOptions } from '../common/builds-items/builds-items-options.model';
-import { filter } from 'rxjs/operators';
 
 @UntilDestroy()
 @Component({
@@ -21,25 +20,25 @@ export class IndexComponent implements OnInit {
       this.router.navigate(['builds/'], { queryParams: { type: 'latest' } });
     }
 
-    this.route.queryParams
-      .pipe(
-        filter(params => params.type && params.type !== this.options.type),
-        untilDestroyed(this)
-      )
-      .subscribe(params => {
-        this.options = { type: params.type };
+    this.route.queryParams.pipe(untilDestroyed(this)).subscribe(params => {
+      if (!params || !params.type) {
+        this.router.navigate(['builds/'], { queryParams: { type: 'latest' } });
+        return;
+      }
 
-        switch (params.type) {
-          case 'latest':
-            this.title = 'Latest';
-            break;
-          case 'commits':
-            this.title = 'Commits';
-            break;
-          case 'pull-requests':
-            this.title = 'Pull Requests';
-            break;
-        }
-      });
+      this.options = { type: params.type };
+
+      switch (params.type) {
+        case 'latest':
+          this.title = 'Latest';
+          break;
+        case 'commits':
+          this.title = 'Commits';
+          break;
+        case 'pull-requests':
+          this.title = 'Pull Requests';
+          break;
+      }
+    });
   }
 }
