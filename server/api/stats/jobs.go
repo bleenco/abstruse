5858878afbabf3ee375b12758546a96f24a97686
Jan Kuri <jkuri88@gmail.com>
2020-12-18 12:20:09 +0100

    fix(stats): job date to minor fix

--- server/api/stats/jobs.go
+++ server/api/stats/jobs.go
@@ -25,6 +25,9 @@ func HandleJobs(jobs core.JobStore) http.HandlerFunc {
 			tto = time.Now()
 		}
 
+		tfrom = time.Date(tfrom.Year(), tfrom.Month(), tfrom.Day(), 0, 0, 0, 0, time.UTC)
+		tto = time.Date(tto.Year(), tto.Month(), tto.Day(), 23, 59, 59, 0, time.UTC)
+
 		jobs, err := jobs.List(tfrom, tto)
 		if err != nil {
 			render.InternalServerError(w, err.Error())
