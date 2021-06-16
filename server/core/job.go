9ef56b76ab54c0393abdee245fc5d60d5557be24
cyan <cyjan@mrcyjanek.net>
2021-06-16 19:44:37 +0200

    fix(core): build failed to start
    When a new build was created, `env` in `jobs` table was `varchar(255)`, which was able to accept only 255 characters.

--- server/core/job.go
+++ server/core/job.go
@@ -8,7 +8,7 @@ type (
 		ID        uint       `gorm:"primary_key;auto_increment;not null" json:"id"`
 		Commands  string     `sql:"type:text" json:"commands"`
 		Image     string     `json:"image"`
-		Env       string     `json:"env"`
+		Env       string     `sql:"type:text" json:"env"`
 		StartTime *time.Time `json:"startTime"`
 		EndTime   *time.Time `json:"endTime"`
 		Status    string     `gorm:"not null;size:20;default:'queued'" json:"status"` // queued | running | passing | failing
