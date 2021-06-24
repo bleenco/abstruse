d5779503e2046cdc5b8714705940f01bc772eb4e
Jan Kuri <jkuri88@gmail.com>
2021-06-24 20:45:11 +0200

    fix(ui): fix inner section for mounting volumes and add information message

--- web/abstruse/src/app/repos/settings-mount/mount-variable.model.ts
+++ web/abstruse/src/app/repos/settings-mount/mount-variable.model.ts
@@ -1,12 +1,8 @@
 export class MountVariable {
-  constructor(
-    public id?: number,
-    public host?: string,
-    public container?: string
-  ) {}
+  constructor(public id?: number, public host?: string, public container?: string) {}
 
   get val(): string {
-    return this.host+":"+this.container;
+    return `${this.host}:${this.container}`;
   }
 }
 
