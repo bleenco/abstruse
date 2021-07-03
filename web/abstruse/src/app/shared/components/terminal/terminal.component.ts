75eaed7b2d9cf12db44b45348db53a87d76ada7b
Jan Kuri <jkuri88@gmail.com>
2021-07-03 17:38:48 +0200

    feat(console): switch between light or dark theme on terminal output

--- web/abstruse/src/app/shared/components/terminal/terminal.component.ts
+++ web/abstruse/src/app/shared/components/terminal/terminal.component.ts
@@ -5,10 +5,61 @@ import {
   Input,
   ElementRef,
   OnChanges,
-  ViewEncapsulation
+  SimpleChanges
 } from '@angular/core';
 import { ITheme, Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
+
+export type TerminalTheme = 'light' | 'dark';
+
+const themes: { [key: string]: ITheme } = {
+  light: {
+    foreground: '#615f51',
+    background: '#ffffff',
+    black: '#050505',
+    red: '#b0263f',
+    green: '#4b862c',
+    yellow: '#d69e2e',
+    blue: '#3b62d9',
+    magenta: '#a431c4',
+    cyan: '#178262',
+    white: '#fbf1bc',
+    brightBlack: '#0e0e0c',
+    brightRed: '#b72424',
+    brightGreen: '#4b862c',
+    brightYellow: '#d69e2e',
+    brightBlue: '#3b62d9',
+    brightMagenta: '#a431c4',
+    brightCyan: '#178262',
+    brightWhite: '#fbf1bc',
+    cursor: 'rgba(0, 0, 0, 0)',
+    cursorAccent: 'rgba(0, 0, 0, 0)',
+    selection: 'rgba(0, 0, 0, 0)'
+  },
+  dark: {
+    foreground: 'hsl(220, 14%, 71%)',
+    background: '#2f3136',
+    black: '#000',
+    red: '#be5046',
+    green: '#98c379',
+    yellow: '#e6c07b',
+    blue: '#61aeee',
+    magenta: '#c678dd',
+    cyan: '#56b6c2',
+    white: '#ffffff',
+    brightBlack: '#666666',
+    brightRed: '#e06c75',
+    brightGreen: '#98c379',
+    brightYellow: '#e6c07b',
+    brightBlue: '#61aeee',
+    brightMagenta: '#c678dd',
+    brightCyan: '#56b6c2',
+    brightWhite: '#ffffff',
+    cursor: 'rgba(0, 0, 0, 0)',
+    cursorAccent: 'rgba(0, 0, 0, 0)',
+    selection: 'rgba(0, 0, 0, 0)'
+  }
+};
 
 @Component({
   selector: 'app-terminal',
@@ -17,6 +68,7 @@ import { FitAddon } from 'xterm-addon-fit';
 })
 export class TerminalComponent implements OnInit, OnDestroy, OnChanges {
   @Input() data: string | null = null;
+  @Input() theme: TerminalTheme = 'light';
 
   terminal: Terminal;
   fitAddon: FitAddon;
@@ -45,7 +97,7 @@ export class TerminalComponent implements OnInit, OnDestroy, OnChanges {
     selection: 'rgba(0, 0, 0, 0)'
   };
 
-  theme: ITheme = {
+  themeDark: ITheme = {
     foreground: 'hsl(220, 14%, 71%)',
     background: '#2f3136',
     black: '#000',
@@ -71,11 +123,13 @@ export class TerminalComponent implements OnInit, OnDestroy, OnChanges {
 
   constructor(public elementRef: ElementRef) {
     this.terminal = new Terminal({
+      convertEol: true,
       allowTransparency: true,
       disableStdin: true,
-      scrollback: 10000,
+      scrollback: 1000000,
       drawBoldTextInBrightColors: true,
-      fontWeightBold: 'bold'
+      fontWeight: 400,
+      fontWeightBold: 700
     });
     this.fitAddon = new FitAddon();
     this.terminal.loadAddon(this.fitAddon);
@@ -85,22 +139,31 @@ export class TerminalComponent implements OnInit, OnDestroy, OnChanges {
     this.terminal.open(this.elementRef.nativeElement.querySelector('.terminal-container'));
     this.terminal.setOption(
       'fontFamily',
-      'Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'
+      'SourceCodePro, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'
     );
     this.terminal.setOption('fontSize', 13);
     this.terminal.setOption('fontWeight', 400);
-    this.terminal.setOption('fontWeightBold', 400);
-    this.terminal.setOption('theme', this.themeLight);
+    this.terminal.setOption('fontWeightBold', 700);
+    this.setTheme();
     this.fitAddon.fit();
-    this.terminal.onData(() => {
-      this.fitAddon.fit();
-    });
+    this.terminal.onData(() => this.fitAddon.fit());
+    this.terminal.onResize(() => this.fitAddon.fit());
+    this.terminal.onLineFeed(() => this.fitAddon.fit());
   }
 
-  ngOnChanges(): void {
-    if (!this.data || !this.terminal) {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!this.terminal) {
       return;
     }
+
+    if ('theme' in changes) {
+      this.setTheme();
+    }
+
+    if (!this.data) {
+      return;
+    }
+
     if (this.data === '__CLEAR__') {
       this.terminal.clear();
       this.terminal.reset();
@@ -113,5 +176,9 @@ export class TerminalComponent implements OnInit, OnDestroy, OnChanges {
   ngOnDestroy(): void {
     this.data = null;
     this.terminal.dispose();
+  }
+
+  private setTheme(): void {
+    this.terminal.setOption('theme', themes[this.theme]);
   }
 }
